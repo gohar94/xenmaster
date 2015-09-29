b68c5eff6e5c5ea476e2327d38d827dea0f30cca
gohar94 <goharirfan94@gmail.com>
2015-09-29 14:45:38 +0500

    append to list.

--- xen_source/multi.c
+++ xen_source/multi.c
@@ -3240,7 +3240,7 @@ static int sh_page_fault(struct vcpu *v,
     // tmp = (struct mappings_list *)malloc(sizeof(struct mappings_list));    
     add.page_number = (va & PAGE_MASK);
     add.frame_number = mfn_x(gmfn);
-    // list_add(&(add.list), &(list_local.list));
+    list_add(&(add.list), &(list_local.list));
     printk("added to list\n");
     
     // @xenmaster
